70accfd6aa1cc7f613354b8778b3fae5f9bc60ee
eugefervi <49234710+eugefervi@users.noreply.github.com>
2022-03-22 22:00:59 +0100

    changes in the names

--- exceptions.py
+++ exceptions.py
@@ -8,8 +8,8 @@ Created on Sat Jul 17 13:46:02 2021
 class GeneralError(Exception):
     """General Error"""
     
-class IDEmpty(Exception):
+class IdEmpty(Exception):
     """IDEmpty: The ID is not valid, please, use a valid ID"""
     
-class CountryIDNotFound(Exception):
+class CountryIdNotFound(Exception):
     """The country ID param it is not found, check this parameter"""
--- info.py
+++ info.py
@@ -11,40 +11,40 @@ from bs4 import BeautifulSoup as bs
 
 class Info(Headers):
     __html= ""
-    __countriesID = "countriesUL"
+    __countries_id = "countriesUL"
     __countries = {}
     __stocks = {}
     __sector = {}
     
     def __init__(self):
         Headers.__init__(self, path = "/stock-screener/")
-        self.__html = bs(self.doGet.text, 'html.parser')
-        self.getCountries()
+        self.__html = bs(self.do_get.text, 'html.parser')
+        self.get_countries()
     
-    def getCountries(self):
-        table = self.__html.find(id = self.__countriesID).find_all('li')
+    def get_countries(self):
+        table = self.__html.find(id = self.__countries_id).find_all('li')
         self.__countries = {country.text[2:-1]: int(country['data-value']) for country in table} # I think that this it is not necessarry, @review
         
         
-    def getSector(self):
+    def get_sector(self):
         # We have to review the html for decide if it is do posible all in one time
         pass
     
-    def getStocksNames(self, countryID):
-        if not countryID: raise IDEmpty
+    def get_stocks_names(self, country_id):
+        if not country_id: raise IdEmpty
         # I think that it is necesarry pass a country ID. No do all in one time
         pass
     
-    def getAllStockNames(self):
+    def get_all_stock_names(self):
         # This function shouldn't use 
         pass
     
     @property
-    def InfoCountries(self):
+    def info_countries(self):
         return self.__countries
     
     @property
-    def InfoSector(self):
+    def info_sector(self):
         return self.__sector 
     
     def __iter__(self):
